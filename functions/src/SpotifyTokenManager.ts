9c6acf167222546096410ecb43ae19f1269db6b3
BartonDev <abartondeveloper@gmail.com>
2020-11-14 11:26:57 -0500

    Fetch spotify credentials from firestore

--- functions/src/SpotifyTokenManager.ts
+++ functions/src/SpotifyTokenManager.ts
@@ -1,4 +1,4 @@
-import {SPOTIFY_CLIENT_SECRET, SPOTIFY_CLIENT_ID} from "./credentials"
+import * as admin from 'firebase-admin';
 
 const fetch = require('cross-fetch')
 
@@ -12,44 +12,75 @@ export class SpotifyToken {
 
 export function getSpotifyToken (): any {
     return new Promise (function (resolve, reject) {
-        const clientSecret = SPOTIFY_CLIENT_SECRET
-        const clientId = SPOTIFY_CLIENT_ID
 
-        const url = 'https://accounts.spotify.com/api/token'
-        const options = {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: `grant_type=client_credentials&client_secret=${clientSecret}&client_id=${clientId}`
-        }
+        if (!admin.apps.length) {
+            admin.initializeApp();
+        } 
 
-        fetch(url, options)
-        .then((res:any) => res.json())
-        .then((data:any) => {
-            let spotifyToken = new SpotifyToken(<TokenResponse> data)
-            resolve(spotifyToken)
+        admin.firestore().collection("credentials").doc("spotify").get()
+        .then(function(doc:any) {
+
+            if (doc.exists) {
+                const data = doc.data()
+                const clientSecret = data["client_secret"]
+                const clientId = data["client_id"]
+                const url = 'https://accounts.spotify.com/api/token'
+                const options = {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    body: `grant_type=client_credentials&client_secret=${clientSecret}&client_id=${clientId}`
+                }
+        
+                fetch(url, options)
+                .then((res:any) => res.json())
+                .then((data:any) => {
+                    let spotifyToken = new SpotifyToken(<TokenResponse> data)
+                    resolve(spotifyToken)
+                })
+                .catch((error:Error) =>{
+                    reject(error)
+                })
+
+
+            } else {
+                reject("Error: could not fetch spotify credentials")
+            }
         })
-        .catch((error:Error) =>{
+        .catch(function(error:Error) {
             reject(error)
-        })
+        });
     })
 }
 
-export function getSpotifyAuthCodeUrl (): string {
-    const clientId = SPOTIFY_CLIENT_ID;
-    const redirectUri = 'http://recordExchange.app/spotifyCallback'; 
-    const state = "";
-    const scope = 'user-read-private playlist-modify-private';
+export function getSpotifyAuthCodeUrl (): any {
+    return new Promise (function (resolve, reject) {
+        if (!admin.apps.length) {
+            admin.initializeApp();
+        } 
 
-    var url = 'https://accounts.spotify.com/authorize';
-    url += '?response_type=token';
-    url += '&client_id=' + encodeURIComponent(clientId);
-    url += '&scope=' + encodeURIComponent(scope);
-    url += '&redirect_uri=' + encodeURIComponent(redirectUri);
-    url += '&state=' + encodeURIComponent(state);
-
-    return (url)
+        admin.firestore().collection("credentials").doc("spotify").get()
+        .then(function(doc:any) {
+            if (doc.exists) {
+                const clientId = doc.data()["client_id"];
+                const redirectUri = 'http://recordExchange.app/spotifyCallback'; 
+                const state = "";
+                const scope = 'user-read-private playlist-modify-private';
+            
+                var url = 'https://accounts.spotify.com/authorize';
+                url += '?response_type=token';
+                url += '&client_id=' + encodeURIComponent(clientId);
+                url += '&scope=' + encodeURIComponent(scope);
+                url += '&redirect_uri=' + encodeURIComponent(redirectUri);
+                url += '&state=' + encodeURIComponent(state);
+            
+                resolve (url)
+            } else {
+                reject("Error: could not fetch spotify credentials")
+            }
+        })
+    })
 }
 
 interface TokenResponse {
